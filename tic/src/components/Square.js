0c04771202f75e0ccd52c7fb0fbe04eec6e98ec1
Schrese <53787633+Schrese@users.noreply.github.com>
2020-04-07 15:27:47 -0400

    Finished

---
+++ tic/src/components/Square.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const style = {
+    background: 'lightblue', 
+    border: '2px solid darkblue',
+    fontSize: '30px',
+    fontWeight: '800', 
+    cursor: 'pointer', 
+    outline: 'none'
+}
+
+const Square = ({ value, onClick }) => 
+// don't need return because arrow functions have an implicit return
+
+    (
+        <button onClick = {onClick} style = {style} >
+            {value}
+        </button>
+    )
+
+
+export default Square;
